a1340e162a3aa3cc6d2d68ae2c2b5d1a9000183f
jennypham <64534681+jennyberry@users.noreply.github.com>
2022-10-27 11:18:21 +1100

    feedback and client section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { card } from '../assets'
+import { feedback } from "../constants"
+import styles, { layout } from '../style'
+import FeedbackCard from './FeedBackCard'
+
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='clients' className={`${styles.paddingY}${styles.flexCenter} flex-cpl relative`}>
+      {/* gradient start */}
+      <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
+      {/* gradient end */}
+      <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
+      <h2 className={styles.heading2}>
+      What people are  <br className='sm:block hidden' />
+      saying about us
+        </h2>
+        <div className='w-full md:mt-0 mt-6'>
+        <p className={`${styles.paragraph} max-w-[450px] text-left`}>
+            Everything you need to accept card payments and grow your business anywhere on the planet.
+          </p>
+          </div>
+      </div>
+      <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card}/>
+        ))}
+      </div>
+      </section >
   )
 }
 
